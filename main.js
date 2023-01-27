f3770fa31a4b0c1ba76501aaa88495fc344d8564
Stella <efun23@outlook.com>
2023-01-27 19:13:13 +0000

    im calculating

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-const input = '1+1'; // input the expression here
-const inputArray = char => {
-  // guh
-}
-// input = [input.split('')];
+const input = '1+2'; // input the expression here
 
-input.forEach(el => {
-  console.log(el);
+
+let irray;
+irray = input.split('');
+
+irray.forEach((el) => {
+  if (el === '1' || el === '2' || el === '3' || el === '4' || el === '5' || el === '6' || el === '7' || el === '8' || el === '9' || el === '0') {
+    console.log(Number(el));
+  } else if (el === '+') {
+    // guh3
+  } else {
+    // .ghuhg
+  }
 });
