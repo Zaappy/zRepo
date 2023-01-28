fa2d8b34cc8ab2d4b43e581cb649db800d81e18b
Stella <efun23@outlook.com>
2023-01-28 00:57:15 +0000

    no its not done yet

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
-const input = '1+2'; // input the expression here
+let input = ['1+2']; // input the expression here
 
 
-let irray;
-irray = input.split('');
-
-irray.forEach((el) => {
-  if (el === '1' || el === '2' || el === '3' || el === '4' || el === '5' || el === '6' || el === '7' || el === '8' || el === '9' || el === '0') {
-    console.log(Number(el));
-  } else if (el === '+') {
-    // guh3
-  } else {
-    // .ghuhg
+input = input[0].split('');
+var irray = [];
+for (let inpDex = 0; inpDex < input.length; inpDex++) {
+  const inp = input[inpDex];
+  if (
+    inp === '1' ||
+    inp === '2' ||
+    inp === '3' ||
+    inp === '4' ||
+    inp === '5' ||
+    inp === '6' ||
+    inp === '7' ||
+    inp === '8' ||
+    inp === '9' ||
+    inp === '0'
+  ) {
+    irray.push(Number(inp));
+  } else if (inp === '+') {
+    input.splice(input.indexOf('+'), 1);
+    var addition = true;
+  } else if (inp === '-') {
+    var subtraction = true;
+  } else if (inp === '*') {
+    var multiplication = true;
+  } else if (inp === '/') {
+    var division = true;
   }
-});
+}
+
+if (addition) {
+  console.log(irray[0] + irray[1]);
+  //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaaaaaaaa
+}
+
+// console.log(input);
+// console.log(irray);
