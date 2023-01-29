8d774655a7b519f3f24c62a2e240eea05024b1d3
Stella <efun23@outlook.com>
2023-01-29 07:22:27 +0000

    FUNctioning calculalator

--- main.js
+++ main.js
@@ -1,39 +1,63 @@
-let input = ['1+2']; // input the expression here
+// console.log(1 + 1);
+// NO I WONT
+// I WANT TO OVERTHINK THIS AS MUCH AS POSSIBLE
+// :D !!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
-input = input[0].split('');
-var irray = [];
-for (let inpDex = 0; inpDex < input.length; inpDex++) {
-  const inp = input[inpDex];
+// Input the expression here.
+let input = ['1 + 1']; 
+// (currently only functions with single-digit numbers) ({[I will fix that tho]})
+// (other numbers will just be concatenated)
+// (i warned you)
+// (dont do it!!!!!)
+
+
+
+// cod e
+let addition;
+let subtraction;
+let multiplication;
+let division;
+input = input[0].split(' ');
+input.forEach((element) => {
   if (
-    inp === '1' ||
-    inp === '2' ||
-    inp === '3' ||
-    inp === '4' ||
-    inp === '5' ||
-    inp === '6' ||
-    inp === '7' ||
-    inp === '8' ||
-    inp === '9' ||
-    inp === '0'
+    element === '1' ||
+    element === '2' ||
+    element === '3' ||
+    element === '4' ||
+    element === '5' ||
+    element === '6' ||
+    element === '7' ||
+    element === '8' ||
+    element === '9' ||
+    element === '0'
   ) {
-    irray.push(Number(inp));
-  } else if (inp === '+') {
-    input.splice(input.indexOf('+'), 1);
-    var addition = true;
-  } else if (inp === '-') {
-    var subtraction = true;
-  } else if (inp === '*') {
-    var multiplication = true;
-  } else if (inp === '/') {
-    var division = true;
+    input[input.indexOf(element)] = Number(element);
+  } else if (element === '+') {
+    addition = true;
+  } else if (element === '-') {
+    subtraction = true;
+  } else if (element === '*') {
+    multiplication = true;
+  } else if (element === '/') {
+    division = true;
   }
-}
+});
+console.log(input);
 
 if (addition) {
-  console.log(irray[0] + irray[1]);
-  //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaaaaaaaa
+  input.splice(input.indexOf('+'), 1);
+  console.log(input[0] + input[1]);
 }
-
-// console.log(input);
-// console.log(irray);
+if (subtraction) {
+  input.splice(input.indexOf('-'), 1);
+  console.log(input[0] - input[1]);
+}
+if (multiplication) {
+  input.splice(input.indexOf('*'), 1);
+  console.log(input[0] * input[1]);
+}
+if (division) {
+  input.splice(input.indexOf('/'), 1);
+  console.log(input[0] / input[1]);
+}
