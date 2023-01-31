ec6f245bd14c913c6870f38c84311e288c5b76e4
Stella <efun23@outlook.com>
2023-01-31 20:55:08 +0000

    mostly working calculator owo

--- main.js
+++ main.js
@@ -1,19 +1,8 @@
-// console.log(1 + 1);
-// NO I WONT
-// I WANT TO OVERTHINK THIS AS MUCH AS POSSIBLE
-// :D !!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
 // Input the expression here.
-let input = ['1 + 1']; 
-// (currently only functions with single-digit numbers) ({[I will fix that tho]})
-// (other numbers will just be concatenated)
-// (i warned you)
-// (dont do it!!!!!)
+let input = ['1 + 1'];
 
 
-
-// cod e
+// (code waaoooow)
 let addition;
 let subtraction;
 let multiplication;
@@ -21,16 +10,16 @@ let division;
 input = input[0].split(' ');
 input.forEach((element) => {
   if (
-    element === '1' ||
-    element === '2' ||
-    element === '3' ||
-    element === '4' ||
-    element === '5' ||
-    element === '6' ||
-    element === '7' ||
-    element === '8' ||
-    element === '9' ||
-    element === '0'
+    element.includes('1') ||
+    element.includes('2') ||
+    element.includes('3') ||
+    element.includes('4') ||
+    element.includes('5') ||
+    element.includes('6') ||
+    element.includes('7') ||
+    element.includes('8') ||
+    element.includes('9') ||
+    element.includes('0')
   ) {
     input[input.indexOf(element)] = Number(element);
   } else if (element === '+') {
@@ -61,3 +50,27 @@ if (division) {
   input.splice(input.indexOf('/'), 1);
   console.log(input[0] / input[1]);
 }
+
+
+// couldnt you just `console.log(1 + 1);`
+// NO!!!!!!!!!!!
+// I WILL NOT !!
+// I WANT TO OVERTHINK THIS AS MUCH AS POSSIBLE
+// :D !!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// Other Notes:
+// currently only functions with single-digit numbers (I will fix that tho)
+// (other numbers will just be concatenated)
+// (i warned you)
+// (dont do it!!!!!)
+// 
+// ACTUALLY!!!
+// NOT TRUE ANYMORE!!
+// Because it works with all numbers now !!
+// Everyone is included !!!!!!!!!
+//
+// More updates to come :>
+
+
+// "Calculatrice."
+// -Z
